99e00047519223bebad5d3ce1c8ac21397cc2414
Igor J.L. Ndiramiye <nigorjeanluc@gmail.com>
2023-08-18 20:37:45 +0300

    Finish the contact page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,7 +2,7 @@ import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
 import { useState, useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCss3, faHtml5, faJsSquare, faLaravel, faNodeJs, faReact } from '@fortawesome/free-brands-svg-icons'
+import { faDocker, faGitAlt, faJsSquare, faLaravel, faNode, faReact } from '@fortawesome/free-brands-svg-icons'
 import Loader from 'react-loaders'
 
 const About = () => {
@@ -24,7 +24,7 @@ const About = () => {
                         />
                     </h1>
                     <p>
-                        I'm a very ambitious front-end developer looking for a role in an
+                        I'm a very ambitious front-end developer & devOps engineer looking for a role in an
                         established IT company with the opportunity to work with the latest
                         technologies on challenging and diverse projects.
                     </p>
@@ -33,31 +33,30 @@ const About = () => {
                         improving my chops one design problem at a time.
                     </p>
                     <p>
-                        If I need to define myself in one sentence that would be a family
-                        person, father of a beautiful daughter, a sports fanatic,
-                        photography enthusiast, and tech-obsessed!!!
+                        If I need to define myself in one sentence that would be an avid coder with an appreciation for basketball, music, 
+                        particularly in the hip-hop genre, currently unattached, photography enthusiast, and tech-obsessed!!!
                     </p>
                 </div>
 
                 <div className='stage-cube-cont'>
                     <div className='cubespinner'>
                         <div className='face1'>
-                            <FontAwesomeIcon icon={faLaravel} color='red' />
+                            <FontAwesomeIcon icon={faJsSquare} color='#EFD81D' />
                         </div>
                         <div className='face2'>
-                            <FontAwesomeIcon icon={faHtml5} color='#F06529' />
+                            <FontAwesomeIcon icon={faGitAlt} color='#EC4D28' />
                         </div>
                         <div className='face3'>
-                            <FontAwesomeIcon icon={faCss3} color='#28A4D9' />
+                            <FontAwesomeIcon icon={faDocker} color='#28A4D9' />
                         </div>
                         <div className='face4'>
                             <FontAwesomeIcon icon={faReact} color='#5ED4F4' />
                         </div>
                         <div className='face5'>
-                            <FontAwesomeIcon icon={faJsSquare} color='#EFD81D' />
+                            <FontAwesomeIcon icon={faLaravel} color='#F05340' />
                         </div>
                         <div className='face6'>
-                            <FontAwesomeIcon icon={faNodeJs} color='#3c873a' />
+                            <FontAwesomeIcon icon={faNode} color='#3c873a' />
                         </div>
                     </div>
                 </div>
