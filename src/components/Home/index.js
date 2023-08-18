1c2534e4d07301e34e184604a568c624d841612b
Igor J.L. Ndiramiye <nigorjeanluc@gmail.com>
2023-08-18 14:34:11 +0300

    Finish About page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,6 +5,7 @@ import LogoTitle from '../../assets/images/logo-s.png'
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
 import Logo from './Logo'
+import Loader from 'react-loaders'
 
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
@@ -12,43 +13,44 @@ const Home = () => {
     const jobArray1 = ['f', 'u', 'l', 'l', 's', 't', 'a', 'c', 'k', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r']
     const jobArray2 = ['&', ' ', 'd', 'e', 'v', 'O', 'p', 's', ' ', 'e', 'n', 'g', 'i', 'n', 'e', 'e', 'r', '.']
 
-    // useEffect(() => {
-    //     return setTimeout(() => {
-    //         setLetterClass('text-animate-hover')
-    //     }, 4000)
-    // }, [])
+    useEffect(() => {
+        setLetterClass('text-animate-hover')
+    }, [])
 
     return (
-        <div className="container home-page">
-            <div className="text-zone">
-                <h1>
-                    <span className={letterClass}>H</span>
-                    <span className={`${letterClass} _12`}>i,</span>
+        <>
+            <div className="container home-page">
+                <div className="text-zone">
+                    <h1>
+                        <span className={letterClass}>H</span>
+                        <span className={`${letterClass} _12`}>i,</span>
+                        <br />
+                        <span className={`${letterClass} _13`}>I</span>
+                        <span className={`${letterClass} _14`}>'m</span>
+                    <img src={LogoTitle} alt="developer" />
+                    <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={nameArray}
+                        idx={16}
+                    />
                     <br />
-                    <span className={`${letterClass} _13`}>I</span>
-                    <span className={`${letterClass} _14`}>'m</span>
-                <img src={LogoTitle} alt="developer" />
-                <AnimatedLetters
-                    letterClass={letterClass}
-                    strArray={nameArray}
-                    idx={16}
-                />
-                <br />
-                <AnimatedLetters
-                    letterClass={letterClass}
-                    strArray={jobArray1}
-                    idx={23}
-                /> <br /> 
-                <AnimatedLetters
-                    letterClass={letterClass}
-                    strArray={jobArray2}
-                    idx={42}
-                /></h1>
-                <h2>Fullstack developer / JavaScript Expert / DevOps engineer</h2>
-                <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+                    <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={jobArray1}
+                        idx={23}
+                    /> <br /> 
+                    <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={jobArray2}
+                        idx={43}
+                    /></h1>
+                    <h2>Fullstack developer / JavaScript Expert / DevOps engineer</h2>
+                    <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+                </div>
+                <Logo />
             </div>
-            <Logo />
-        </div>
+            <Loader type="pacman"/>
+        </>
     )
 }
 
